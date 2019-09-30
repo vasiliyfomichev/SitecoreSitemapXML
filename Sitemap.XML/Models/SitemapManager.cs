026d2527674cce6b8fee75ad2ff96870088922d0
Mario Guillen <mguillen@oshyn.com>
2019-09-30 14:46:40 -0500

    Commit for ticket MBREQ-297

--- Sitemap.XML/Models/SitemapManager.cs
+++ Sitemap.XML/Models/SitemapManager.cs
@@ -431,10 +431,21 @@ namespace Sitemap.XML.Models
             if (string.IsNullOrWhiteSpace(_config.FileName)) return;
             string robotsPath = MainUtil.MapPath(string.Concat("/", Constants.RobotsFileName));
             StringBuilder sitemapContent = new StringBuilder(string.Empty);
-            sitemapContent.AppendLine("User-agent: *");
-            sitemapContent.AppendLine("Disallow:");
+            
 
-            StreamWriter sw = new StreamWriter(robotsPath, false);
+            if (File.Exists(robotsPath))
+            {
+	            StreamReader sr = new StreamReader(robotsPath);
+	            sitemapContent.Append(sr.ReadToEnd());
+	            sr.Close();
+            }
+            else
+            {
+	            sitemapContent.AppendLine("User-agent: *");
+	            sitemapContent.AppendLine("Disallow:");
+			}
+
+			StreamWriter sw = new StreamWriter(robotsPath, false);
             string sitemapUrl = _config.ServerUrl + "/" + _config.FileName;
             string sitemapLine = string.Concat("Sitemap: ", sitemapUrl);
             if (!sitemapContent.ToString().Contains(sitemapLine))
